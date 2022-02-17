7c7cbbb2c1c8b4b3a8c0ea3369cd9c18906feb7b
Infinity <itsanshugoyal@gmail.com>
2022-02-17 10:58:19 +0530

    stup app for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
@@ -127,6 +127,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+app.listen(port, function () {
+  console.log(`server started at port ${port}`);
 });
